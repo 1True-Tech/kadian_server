9d7568a5c5f18c2d70dee050356c738ebeee0e58
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-09-19 16:28:55 +0100

    Update delete.js

--- app/routerLogic/orders/[id]/delete.js
+++ app/routerLogic/orders/[id]/delete.js
@@ -14,7 +14,7 @@ export async function deleteOrder(event) {
     label: "Parameters",
   });
 
-  if (hasError || !object) {
+  if (hasError || !params) {
     return {
       statusCode: 400,
       status: "bad",
