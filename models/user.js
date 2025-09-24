467a3d99d793cd21f1ad17e856724961a5c95fb7
p34-pac <peterpaultolulope@gmail.com>
2025-09-24 08:19:28 +0100

    fix enum for user

--- models/user.js
+++ models/user.js
@@ -58,7 +58,7 @@ const UserSchema = new Schema({
   ],
   role: {
     type: String,
-    enum: ["user", "admin" | "superadmin"],
+    enum: ["user", "admin" , "superadmin"],
     default: "user",
   },
   isVerified: {
